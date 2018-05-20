1932860e14c2ca0bbb9475c621382c0511a7873e
Uthaeus <romanlavery@gmail.com>
2018-05-20 16:41:09 -0600

    Write a Ruby program to check two given integers and return the larger value. However if the two values have the same remainder when divided by 5 then return the smaller value and if the two values are the same, return 0.

---
+++ 55.rb
@@ -0,0 +1,25 @@
+# Write a Ruby program to check two given integers and return the larger value. However if the two values have the same remainder when divided by 5 then return the smaller value and if the two values are the same, return 0.
+
+
+def diff num1, num2
+  if num1 == num2
+    return 0
+  elsif num1 % 5 == num2 % 5
+    if num1 > num2
+      return num2
+    else
+      return num1
+    end
+  else
+    if num1 > num2
+      return num1
+    else
+      return num2
+    end
+  end
+end
+
+
+p diff(10, 15)
+p diff(12, 34)
+p diff(123, 321)
