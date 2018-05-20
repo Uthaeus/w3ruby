5590568abeb11e3603b5372f3ef1664d1e9ec93c
Uthaeus <romanlavery@gmail.com>
2018-05-20 11:14:03 -0600

    Write a program to print the multiply of decimal numbers

---
+++ 05.rb
@@ -0,0 +1,12 @@
+# Write a program to print the multiply of decimal numbers
+
+
+puts "Enter first number:"
+num1 = gets.chomp.to_f
+puts
+puts "Enter second number:"
+num2 = gets.chomp.to_f
+
+puts
+result = num1 * num2
+puts result
