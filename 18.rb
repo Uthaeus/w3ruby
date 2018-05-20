73ac1d38414ac0fe6ae87f12af6faa3e1f22b3ef
Uthaeus <romanlavery@gmail.com>
2018-05-20 12:19:11 -0600

    Write a Ruby program to find the maximum of two numbers.

---
+++ 18.rb
@@ -0,0 +1,15 @@
+# Write a Ruby program to find the maximum of two numbers.
+
+
+def larger num1, num2
+  if num1 > num2
+    p num1
+  else
+    p num2
+  end
+end
+
+
+larger(12, 13)
+larger(43, 789)
+larger(789, 43)
