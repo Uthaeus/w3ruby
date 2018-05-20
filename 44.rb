6c75361928157d0e3d91170e53c06b1c00e6452f
Uthaeus <romanlavery@gmail.com>
2018-05-20 15:17:46 -0600

    Write a Ruby program to check two non-negative integer values and return true if they have the same last digit.

---
+++ 44.rb
@@ -0,0 +1,20 @@
+# Write a Ruby program to check two non-negative integer values and return true if they have the same last digit.
+
+
+def same num1, num2
+  n1 = num1.to_s.split('')
+  n2 = num2.to_s.split('')
+
+  if n1[-1] == n2[-1]
+    return true
+  else 
+    return false
+  end
+end
+
+
+p same(12, 22)
+p same(3454, 4444)
+p same(355, 324365)
+p same(2, 4)
+p same(2, 2)
