31076e0e0caaf7fcdf50110d134c8a2cb736db89
Uthaeus <romanlavery@gmail.com>
2018-05-20 20:59:44 -0600

    Write a Ruby program to split a delimited string into an array.

--- arrays/10.rb
+++ arrays/10.rb
@@ -1,13 +1,7 @@
 # Write a Ruby program to split a delimited string into an array.
 
 
-def splitter arr 
-  arr.split(', ')
-end
-
-
-test1 = 'some, stuff, here, to, test'
-test2 = 
+ 
 
 color = "Red, Green, Blue, White"
 nums = "1, 3, 4, 5, 7"
