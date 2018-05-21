ec0d301d2fd9a8022c09577b6415ee4c508796bb
Uthaeus <romanlavery@gmail.com>
2018-05-21 14:31:59 -0600

    Write a Ruby program to get the number of lines in a given string.

---
+++ strings/16.rb
@@ -0,0 +1,10 @@
+# Write a Ruby program to get the number of lines in a given string.
+
+
+str = "Python is a widely used high-level, general-purpose,
+interpreted, dynamic programming language.
+Its design philosophy emphasizes code readability, 
+and its syntax allows programmers to express concepts in
+fewer lines of code than possible in languages such 
+as C++ or Java."
+print "Number of lines of the above string: ",str.lines.count
