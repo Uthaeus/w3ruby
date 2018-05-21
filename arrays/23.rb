49c75c3e477fd853b08b3cbc03dc24fea9d7dad2
Uthaeus <romanlavery@gmail.com>
2018-05-21 09:27:15 -0600

    Write a Ruby program to create a new array of length 4 containing all their elements from two array of integers, length 2.

---
+++ arrays/23.rb
@@ -0,0 +1,13 @@
+# Write a Ruby program to create a new array of length 4 containing all their elements from two array of integers, length 2.
+
+
+def adder arr1, arr2
+  result = arr1 + arr2
+  result
+end
+
+
+t1 = [2, 3]
+t2 = [4, 5]
+
+p adder(t1, t2)
